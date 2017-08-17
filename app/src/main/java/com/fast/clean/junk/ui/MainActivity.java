34817a62fa6365390ac0023ba55dced1616ded0b
JiangFengYong <986143964@qq.com>
2017-08-17 18:03:30 +0800

    update code

--- app/src/main/java/com/fast/clean/junk/ui/MainActivity.java
+++ app/src/main/java/com/fast/clean/junk/ui/MainActivity.java
@@ -73,7 +73,6 @@ public class MainActivity extends BaseActivity implements MainView, DrawerLayout
     TextView main_msg_ram_percent, main_msg_sd_percent, main_msg_cpu_percent;
     com.mingle.widget.LinearLayout ll_ad_full;
     ProgressBar ad_progressbar;
-    TextView main_full_time;
     LinearLayout main_battery;
 
     LottieAnimationView lot_ad;
@@ -394,12 +393,10 @@ public class MainActivity extends BaseActivity implements MainView, DrawerLayout
             if (ll_ad_full != null && nativeView_full != null) {
                 ll_ad_full.addView(nativeView_full);
                 ll_ad_full.setVisibility(View.VISIBLE);
-                nativeView_full.findViewById(R.id.ad_delete).setVisibility(View.GONE);
-                main_full_time = (TextView) nativeView_full.findViewById(R.id.main_full_time);
+                ImageView ad_delete = (ImageView) nativeView_full.findViewById(R.id.ad_delete);
                 LinearLayout loading_text = (LinearLayout) nativeView_full.findViewById(R.id.loading_text);
                 loading_text.setOnClickListener(null);
-                main_full_time.setVisibility(View.VISIBLE);
-                main_full_time.setOnClickListener(new View.OnClickListener() {
+                ad_delete.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         handler.removeCallbacks(fullAdRunnale);
